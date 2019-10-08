cc2c14f59284a1b7000039a48965e2a6a6d623f4
Mrigank Krishan <mrigankkrishan@gmail.com>
2019-10-08 23:40:16 +0530

    use SO_BINDTODEVICE on linux

--- main.go
+++ main.go
@@ -10,10 +10,14 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"runtime"
+	"reflect"
+	"syscall"
 )
 
 type load_balancer struct {
 	address             string
+	ifname              string
 	contention_ratio    int
 	current_connections int
 }
@@ -30,7 +34,7 @@ var mutex *sync.Mutex
 /*
 	Get a load balancer according to contention ratio
 */
-func get_load_balancer() string {
+func get_load_balancer() *load_balancer {
 	mutex.Lock()
 	lb := &lb_list[lb_index]
 	lb.current_connections += 1
@@ -44,7 +48,7 @@ func get_load_balancer() string {
 		}
 	}
 	mutex.Unlock()
-	return lb.address
+	return lb
 }
 
 /*
@@ -71,14 +75,41 @@ func forward_connections(local_conn, remote_conn net.Conn) {
 }
 
 /*
+	bind conn to interface
+*/
+func bindToDevice(conn net.Conn, device string) error {
+	ptrVal := reflect.ValueOf(conn)
+	val := reflect.Indirect(ptrVal)
+	//next line will get you the net.netFD
+	fdmember := val.FieldByName("fd")
+	val1 := reflect.Indirect(fdmember)
+	netFdPtr := val1.FieldByName("sysfd")
+	fd := int(netFdPtr.Int())
+	//fd now has the actual fd for the socket
+	return syscall.SetsockoptString(fd, syscall.SOL_SOCKET,
+		syscall.SO_BINDTODEVICE, device)
+}
+
+/*
 
  */
 func server_response(local_conn net.Conn, address string) {
-	load_balancer_addr := get_load_balancer()
+	load_balancer := get_load_balancer()
+	load_balancer_addr := load_balancer.address
 
 	local_addr, _ := net.ResolveTCPAddr("tcp4", load_balancer_addr)
 	remote_addr, _ := net.ResolveTCPAddr("tcp4", address)
-	remote_conn, err := net.DialTCP("tcp4", local_addr, remote_addr)
+	var remote_conn net.Conn
+	var err error
+	if load_balancer_addr != "" {
+		remote_conn, err = net.DialTCP("tcp4", local_addr, remote_addr)
+	} else {
+		remote_conn, err = net.Dial("tcp4", address)
+		err := bindToDevice(remote_conn, load_balancer.ifname)
+		if err != nil {
+			log.Fatal("[FATAL] Failed to bind socket to interface ", load_balancer.ifname)
+		}
+	}
 
 	if err != nil {
 		log.Println("[WARN]", address, "->", load_balancer_addr, fmt.Sprintf("{%s}", err))
@@ -86,7 +117,7 @@ func server_response(local_conn net.Conn, address string) {
 		local_conn.Close()
 		return
 	}
-	log.Println("[DEBUG]", address, "->", load_balancer_addr)
+	log.Println("[DEBUG]", address, "->", load_balancer_addr, load_balancer.ifname)
 	local_conn.Write([]byte{5, SUCCESS, 0, 1, 0, 0, 0, 0, 0, 0})
 	forward_connections(local_conn, remote_conn)
 }
@@ -136,8 +167,18 @@ func parse_load_balancers(args []string) {
 	for idx, a := range args {
 		splitted := strings.Split(a, "@")
 		var lb_ip = splitted[0]
+		var lb_ifname string
 		if net.ParseIP(lb_ip).To4() == nil {
-			log.Fatal("[FATAL] Invalid address ", lb_ip)
+			// use it as ifname in Linux
+			if runtime.GOOS == "linux" {
+				lb_ifname = lb_ip
+				lb_ip = ""
+				if _, err := net.InterfaceByName(lb_ifname);err!=nil {
+					log.Fatal("[FATAL] Invalid interface name ", lb_ifname)
+				}
+			} else {
+				log.Fatal("[FATAL] Invalid address ", lb_ip)
+			}
 		}
 		var cont_ratio int = 1
 		if len(splitted) > 1 {
@@ -148,8 +189,8 @@ func parse_load_balancers(args []string) {
 			}
 		}
 
-		log.Printf("[INFO] Load balancer %d: %s, contention ratio: %d\n", idx+1, lb_ip, cont_ratio)
-		lb_list[idx] = load_balancer{address: fmt.Sprintf("%s:0", lb_ip), contention_ratio: cont_ratio, current_connections: 0}
+		log.Printf("[INFO] Load balancer %d: %s%s, contention ratio: %d\n", idx+1, lb_ip, lb_ifname, cont_ratio)
+		lb_list[idx] = load_balancer{address: fmt.Sprintf("%s:0", lb_ip), contention_ratio: cont_ratio, current_connections: 0, ifname: lb_ifname}
 	}
 }
 
